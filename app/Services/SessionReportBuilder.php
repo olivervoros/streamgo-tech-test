31f09e98199fc5e7cc0b1f01ab658a2a4da94679
Oliver Voros <olivervoros@gmail.com>
2021-10-29 13:09:01 +0200

    comment

--- app/Services/SessionReportBuilder.php
+++ app/Services/SessionReportBuilder.php
@@ -7,6 +7,7 @@ class SessionReportBuilder
 
     public function getReport()
     {
+        // We could take this data from the DB using eloquent or directly from the DB
         $example = ['SessionData1', 'SessionData2', 'SessionData3', 'SessionData4'];;
         return collect($example);
     }
